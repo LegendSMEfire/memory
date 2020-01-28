cf475b87f67b50c6249e0797d28ec35598351e08
marceau-maubert <marceau.maubert2@gmail.com>
2020-01-28 17:20:50 +0100

    There we go, restore that from memory

--- memory.php
+++ memory.php
@@ -2,15 +2,57 @@
 
 session_start();
 
-/*
-		&& $_POST["pickedCard"] == $id && $_SESSION["matching"] != -1) {
+function startGame() {
+	if (isset($_POST["pairs"]) && is_numeric($_POST["pairs"])) {
+		$_SESSION["game"] = [];
+		$_SESSION["matching"] = -1;
+		$cards = scandir("./assets/cards");
+		array_splice($cards, 0, 2);
+		for ($i = 0; $i < $_POST["pairs"]; $i++) {
+			$randKey = array_rand($cards);
+			$randVal = $cards[$randKey];
+			$card = [ "image" => $randVal, "flipped" => false ];
+			array_push($_SESSION["game"], $card, $card);
+			array_splice($cards, $randKey, 1);
+		}
+		shuffle($_SESSION["game"]);
+	}
+}
+
+$disabled = false;
+function pickCard() {
+	global $disabled;
+
+	if (isset($_POST["pickedCard"]) && is_numeric($_POST["pickedCard"]) && isset($_SESSION["game"][$_POST["pickedCard"]])) {
+		if ($_SESSION["matching"] < 0) {
+			$_SESSION["matching"] = $_POST["pickedCard"];
+		} else {
+			$matchingCard = &$_SESSION["game"][$_SESSION["matching"]];
+			$pickedCard = &$_SESSION["game"][$_POST["pickedCard"]];
+			if ($matchingCard["image"] == $pickedCard["image"] && $_SESSION["matching"] != $_POST["pickedCard"]) {
+				$matchingCard["flipped"] = true;
+				$pickedCard["flipped"] = true;
+				$_SESSION["matching"] = -1;
+			} else {
+				$disabled = true;
+				header("Refresh: 1.5; URL=memory.php");
+			}
+		}
+	}
+}
+
+function getCardFrontState($id) {
+	if ($_SESSION["matching"] == $id) {
+		return "front_selected";
+	} else {
+		if (isset($_POST["pickedCard"]) && $_POST["pickedCard"] == $id && $_SESSION["matching"] != -1) {
 			return "front_wrong";
 		} else {
 			return "front";
 		}
 	}
 }
-*/
+
 function getCardStyle($id) {
 	if (isset($_SESSION["game"]) && isset($_SESSION["game"][$id])) {
 		if ((isset($_POST["pickedCard"]) && $_POST["pickedCard"] == $id) ||
